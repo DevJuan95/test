63290f2952308e8364c865709a18c8135869aa77
DevJuan95 <ing.juanmh@gmail.com>
2021-01-21 10:42:27 -0500

    changing react file estructure

--- api/controllers/trackController.js
+++ api/controllers/trackController.js
@@ -1,9 +1,9 @@
 
-const SpotifyService = require('../services/trackService');
+const TrackService = require('../services/trackService');
 
 exports.tracksSearch = async (req, res) => {
   try {
-    const tracks = await SpotifyService.findTracks('', '');
+    const tracks = await TrackService.findTracks('', '');
     return res.status(200).json({ status: 200, data: tracks, message: "Success" });
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
